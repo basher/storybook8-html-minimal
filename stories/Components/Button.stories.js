fea01c562cab953f3e01ab70b19837ecb24355f8
basher <basher@madasafish.com>
2024-01-03 11:06:06 +0000

    fix show source code in mdx

--- stories/Components/Button.stories.js
+++ stories/Components/Button.stories.js
@@ -1,27 +1,25 @@
 import './button.css';
 
-// More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction
 export default {
-  title: 'Components/Button',
-  argTypes: {
-    label: { control: 'text' },
-    buttonType: {
-      control: 'select',
-      options: ['primary', 'secondary'],
+    title: 'Components/Button',
+    argTypes: {
+        label: { control: 'text' },
+        buttonType: {
+            control: 'select',
+            options: ['primary', 'secondary'],
+        },
     },
-  },
-  render: ({ label, ...args }) => {
-    return `
-      <button class="button button--${args.buttonType}">
-        ${label}
-      </button>
-    `;
-  },
+    render: ({ label, ...args }) => {
+        return `
+<button class="button button--${args.buttonType}">
+    ${label}
+</button>
+        `;
+    },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
 export const TextButton = {
-  args: {
-    label: 'Button',
-  },
+    args: {
+        label: 'Button',
+    },
 };

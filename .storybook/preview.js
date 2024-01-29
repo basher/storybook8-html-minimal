2c8b6db35d043edba580312eb2f6dc0560d04861
basher <basher@madasafish.com>
2024-01-29 10:06:58 +0000

    delete commented lines from preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,6 @@
 /** @type { import('@storybook/html').Preview } */
 const preview = {
   parameters: {
-    // actions: { argTypesRegex: "^on[A-Z].*" },
-    // controls: {
-    //   matchers: {
-    //     color: /(background|color)$/i,
-    //     date: /Date$/,
-    //   },
-    // },
     options: {
       storySort: {
         order: ['Intro', 'Components'],

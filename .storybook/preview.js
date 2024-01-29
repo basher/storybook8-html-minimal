361a2a2621abfc2d25e289481f42e67324bfa267
basher <basher@madasafish.com>
2024-01-29 10:09:59 +0000

    typo

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,12 @@
 /** @type { import('@storybook/html').Preview } */
 const preview = {
-  parameters: {
-    options: {
-      storySort: {
-        order: ['Intro', 'Components'],
-      },
-    }
-  },
+    parameters: {
+        options: {
+            storySort: {
+            order: ['Intro', 'Components'],
+            },
+        },
+    },
 };
 
 export default preview;
